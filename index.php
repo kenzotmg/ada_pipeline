09d4154d7725c6bd66022515841d440df8c80565
Kenzo Tanizaki <kenzotmg1@gmail.com>
2025-09-15 21:39:50 -0300

    triggering actions

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
    
-    <h1> Escreva sua mensagem! </h1>
+    <h1> Escreva sua mensagem!  </h1>
     <form action="process.php" method="POST">
     <input type="text" name="message" id="message">
     <input type="submit" value="Enviar mensagem">
